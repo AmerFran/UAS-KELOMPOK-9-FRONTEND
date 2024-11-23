c442a7cc6e9918f5cce3e1368dac8d02a0b36d9c
jervis535 <jervishuang123@gmail.com>
2024-11-23 05:35:34 +0300

    extra rows, query for api, changing api and jquery implementation on ingredients.html

--- api/converter.js
+++ api/converter.js
@@ -4,6 +4,26 @@ import fetch from 'node-fetch';
 
 const categories = ["Breakfast", "Vegan", "Side", "Seafood"];
 
+//fetch ingredients and measurements
+function details(meal) {
+    const ingredients = [];
+    const measurements = [];
+    const area=meal[`strArea`];
+    const instructions=meal[`strInstructions`];
+
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
+        
+        if (ingredient && ingredient.trim() !== '' && measure && measure.trim() !== '') {
+            ingredients.push(ingredient);
+            measurements.push(measure);
+        }
+    }
+
+    return { ingredients, measurements,area,instructions };
+}
+
 //converts the data from api into the database
 async function fetchAndInsertData() {
     for (let i = 0; i < categories.length; i++) {
@@ -14,27 +34,33 @@ async function fetchAndInsertData() {
             for (let meal of data.meals) {
                 const name = meal.strMeal;
                 const imagelink = meal.strMealThumb;
+                const temp = meal.idMeal;
+
+                //gets detailed response
+                const res=await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${temp}`);
+                const detailed=await res.json();
+                const detailedMeal=detailed.meals[0];
+                const {ingredients,measurements,area,instructions}=details(detailedMeal);
+
                 //prices are randomly generated
                 const price = faker.commerce.price(5, 20, 2);
-
                 //inserts data into the database
                 try {
                     await pool.query(
-                        'INSERT INTO food (name, imagelink, category, price) VALUES ($1, $2, $3, $4)',
-                        [name, imagelink, category, price]
+                        'INSERT INTO food (name, imagelink, category, price,ingredients,measurements,area,instructions) VALUES ($1, $2, $3, $4,$5,$6,$7,$8)',
+                        [name, imagelink, category, price,ingredients,measurements,area,instructions]
                     );
                     console.log(`Inserted meal: ${name} (${category})`);
                 } catch (err) {
                     console.error(`Error inserting meal ${name}:`, err);
                 }
             }
-        } else {
-            console.log(`No meals found for category: ${category}`);
         }
     }
-
     console.log("Data import completed!");
 }
+
+// Start the process of fetching and inserting data
 fetchAndInsertData()
     .catch(err => {
         console.error('Error during data import:', err);
